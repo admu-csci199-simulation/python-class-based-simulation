318b20e11c256c7f1c0f00404587c028c7c80a30
bfrancisco <franciscobryaaan@gmail.com>
2026-02-06 13:58:25 +0800

    add: new field "isShared" on PostInteraction

--- PostInteraction.py
+++ PostInteraction.py
@@ -1,9 +1,12 @@
+# This class is generated after an agent accepted a post via DDC function.
+# Receiving a post and rejecting to share it does NOT count as an agent accepting a post.
 class PostInteraction:
-    def __init__(self, time, agent, agentBelief, post, postInterestValue):
+    def __init__(self, time, agent, agentBelief, post, postInterestValue, isShared):
         self.time = time
         self.agent = agent
         self.agentBelief = agentBelief # agent belief at time t
         self.post = post
         self.postInterestValue = postInterestValue # post interest value at time t
+        self.isShared = isShared
 
-    # create function that generates graphs here
+    
