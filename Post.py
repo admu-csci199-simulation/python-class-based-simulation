3e8fd1ef0496c7679df9211428906b43538317fc
bfrancisco <franciscobryaaan@gmail.com>
2026-03-22 11:57:26 +0800

    feat: added assignment of OP as functions

--- Post.py
+++ Post.py
@@ -1,7 +1,8 @@
 import random
 import Constants
+import Agent
 
-def generatePost(postID, beliefValue=None, originalPoster=None, postingTime=None, interestValue=None, postTopic=None, isMisinformation=None):
+def generatePost(postID, beliefValue=None, originalPoster=None, postingTime=None, interestValue=None, postTopic=None, isMisinformation=None, agentsList=None, agenciesList=None):
     if beliefValue is None:
         beliefValue = random.randint(-4, 4)
     if originalPoster is None:
@@ -15,18 +16,23 @@ def generatePost(postID, beliefValue=None, originalPoster=None, postingTime=None
     if isMisinformation is None:
         isMisinformation = False
     
-    return Post(postID, beliefValue, interestValue, postingTime, originalPoster, postTopic, isMisinformation)
+    return Post(postID, beliefValue, interestValue, postingTime, postTopic, isMisinformation, agentsList, agenciesList)
 
 class Post:
-    def __init__(self, postID, beliefValue, interestValue, postingTime, originalPoster, postTopic, isMisinformation):
+    def __init__(self, postID, beliefValue, interestValue, postingTime, postTopic, isMisinformation, agentsList, agenciesList):
         self.postID = postID
         self.beliefValue = beliefValue
         self.interestValue = interestValue
         self.postingTime = postingTime
-        self.originalPoster = originalPoster
         self.postTopic = postTopic
         self.interactions = []
         self.isMisinformation = isMisinformation
+
+        self.originalPoster = None # index of agent/news agency
+        if isMisinformation:
+            self.originalPoster = self.assignMisinfoOP(agentsList)
+        else:
+            self.originalPoster = self.assignRealNewsOP(agenciesList)
 
     def __repr__(self):
         return f"post_{self.postID}"
@@ -46,3 +52,25 @@ class Post:
                 return beliefName
         
         raise RuntimeError("Agent belief value not in range")
+
+    def assignMisinfoOP(self, agentsList: list[Agent.Agent]):
+        possibleOPs = [] # indeces
+        minBeliefDist = 1e9
+
+        # same belief value
+        for i in range(len(agentsList)):
+            if agentsList[i].beliefValue == self.beliefValue:
+                possibleOPs.append(i)
+            minBeliefDist = min(minBeliefDist, abs(agentsList[i].beliefValue - self.beliefValue))
+        
+        # closest belief distance
+        if len(possibleOPs) == 0:
+            if abs(agentsList[i].beliefValue - self.beliefValue) == minBeliefDist:
+                possibleOPs.append(i)
+        
+        chosenOPid = random.choice(possibleOPs)
+        return chosenOPid
+
+    def assignRealNewsOP(self, agenciesList: list[Agent.Agent]):
+        chosenOPid = random.choice(range(len(agenciesList)))
+        return chosenOPid
