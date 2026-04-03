79937619cc399f52a377213a9ce58e9e0e30b7b2
bfrancisco <franciscobryaaan@gmail.com>
2026-04-03 16:56:22 +0800

    edit config genrtr: copy hyp1 to hyp7

--- Hyp7Configs.py
+++ Hyp7Configs.py
@@ -5,7 +5,7 @@ import random
 
 OUTPUT_FOLDER = "input"
 NUM_FILES = 100               # how many json files to generate
-POSTS_PER_FILE = 100          # number of posts in each file
+POSTS_PER_FILE = 10           # number of posts in each file
 AGENTS_PER_FILE = 1000
 
 def split_into_three(total, min_value=1):
