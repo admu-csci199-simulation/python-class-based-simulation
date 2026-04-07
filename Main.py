f5c35ba8c35a75cc6273240419f943f57eaf6984
bfrancisco <franciscobryaaan@gmail.com>
2026-04-07 10:54:23 +0800

    edit: simData format

--- Main.py
+++ Main.py
@@ -39,7 +39,7 @@ def mapGraphToAgents(agentsData, networkData):
 def simulationProper(configData, networkData, simulationAgentsList: "list[Agent.Agent]", agenciesList: "list[Agent.Agent]"):
     
     postsQueue = readPosts(configData, simulationAgentsList, agenciesList)
-    simulationData = {p.postID : {"isMisinformation" : p.isMisinformation, "interactions" : [0 for i in range(24*60)]} for p in postsQueue} # for hypothesis 6
+    simulationData = {p.postID : {"isMisinformation" : p.isMisinformation, "interactions" : {}} for p in postsQueue} # for hypothesis 6
 
     
     for currentTime in range(Constants.MAXIMUM_TIME):
